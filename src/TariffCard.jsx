2700a65e0730190dc823dd4bc387bf6ebdab0fd0
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-05-19 17:32:20 +0300

    feat: option selected added

--- src/TariffCard.jsx
+++ src/TariffCard.jsx
@@ -1,11 +1,16 @@
 import './tariffCard.css';
+import { useState } from 'react';
 
 function TariffCard(props) {
-    const { headColor, tariffColor, price, isSelected, speed } = props;
-    let cardClass = '';
-    if (isSelected) cardClass = 'selected';
+    const [pressed, setPressed] = useState(false);
+    const { headColor, tariffColor, price, speed } = props;
+
+    const handleChange = () => {
+        setPressed(!pressed)
+    }
+
     return (
-        <div className={`card ${cardClass}`}>
+        <div className={pressed ? 'selected' : ''} onClick={handleChange}>
             <div className={`card__head ${headColor}`}>Безлимитный <span>{price}</span></div>
             <div className={`card__tariff ${tariffColor}`}>
                 <div className="card__tariff-rub">руб</div>
